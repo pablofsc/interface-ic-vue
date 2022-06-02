cd0f614b08878215b6cc56f46bff32c297aa1d0b
pablofsc <pablofsc@outlook.com>
2022-06-02 03:49:40 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <OperatorTable />
+</template>
+
+<script>
+import OperatorTable from './components/OperatorTable.vue';
+
+export default {
+    name: "App",
+
+    components: {
+        OperatorTable,
+    },
+};
+</script>
+
+<style scoped>
+body {
+    background-color: #f1f1f1;
+}
+</style>
